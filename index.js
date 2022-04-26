c9207c169563dddce92e6c986c73224fab757043
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-04-26 17:45:49 -0400

    commit tree 92382246129fbe74b3d721a3f0d2bd139d745840

--- index.js
+++ index.js
@@ -1,25 +1,58 @@
 
 
-function submitData( name, email) {
+function submitData(name, email){
     return fetch("http://localhost:3000/users", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        },
-        body: JSON.stringify({
-                name,
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                name, 
                 email
-        })
+            })
     })
     .then( function ( response ) {
-        debugger
+
         return response.json()
       } )
       .then( function ( object ) {
+        console.log(object)  
         document.body.innerHTML = object[ "id" ]
       } )
-    //   .catch( function ( error ) {
-    //     document.body.innerHTML = error.message
-    //   } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
 }
+
+
+
+
+
+
+
+
+// function submitData( name, email) {
+//     return fetch("http://localhost:3000/users", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json"
+//         },
+//         body: JSON.stringify({
+//                 name,
+//                 email
+//         })
+//     })
+//     .then( function ( response ) {
+
+//         return response.json()
+//       } )
+//       .then( function ( object ) {
+//         console.log(object)  
+//         document.body.innerHTML = object[ "id" ]
+//       } )
+//       .catch( function ( error ) {
+//         document.body.innerHTML = error.message
+//       } )
+// }
